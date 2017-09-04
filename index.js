ff1427f2b1de03355b6e74bbe45811ba22673c35
acetranslink <acetranslink@gmail.com>
2017-09-04 00:06:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(){
   return ("Happy holidays!")
 }// write your code below!
 function happyHolidaysTo(name){
-  return `Happy Holidays, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
